b7d18506012a1fe3c461f6f081b7f6ac44c2820c
Juan Pineda <juancanales5019@gmail.com>
2025-01-24 20:32:16 -0600

    Completed basic layout of the page

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('download-guide').addEventListener('click', function () {
+    alert('Thanks for your interest! Your free guide will be available soon.');
+  });
+  
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Thank you for reaching out! We will get back to you soon.');
+  });
+  
